17820f72f7895bba523ea9b6bc21a03a49f17828
Joshua Gonzalez Romero <69594954+secondglitter@users.noreply.github.com>
2023-10-31 20:52:54 -0500

    Cambios a la tabla grupos

--- src/group/Dto/update-group.dto.ts
+++ src/group/Dto/update-group.dto.ts
@@ -1,8 +1,15 @@
 export class UpdateGroupDto{
 
     NameGroup?: string;
+    Description?: string;
     Members?: number;
     Class?: string;
+    List1?: string;
+    List2?: string;
+    List3?: string;
+    List4?: string;
     Image?: string;
-    
+    Image1?: string;
+    Image2?: string;
+    Image3?: string;
 }
--- src/group/group.entity.ts
+++ src/group/group.entity.ts
@@ -7,7 +7,10 @@ export class Group {
   
   @Column()
   NameGroup: string;
-  
+
+  @Column()
+  Description: string;
+
   @Column()
   Members: number;
 
@@ -15,6 +18,27 @@ export class Group {
   Class: string;
 
   @Column()
+  List1: string;
+
+  @Column()
+  List2: string;
+
+  @Column()
+  List3: string;
+
+  @Column()
+  List4: string;
+
+  @Column()
   Image: string;
+
+  @Column()
+  Image1: string;
+
+  @Column()
+  Image2: string;
+  
+  @Column()
+  Image3: string;
 
 }
